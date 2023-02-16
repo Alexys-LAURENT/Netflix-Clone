e9a975c2692cde55c784d9c3a4f1eb574e23b413
Alexys LAURENT <alexyslaurent.442@gmail.com>
2023-02-16 21:36:44 +0100

    Start SideBar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,11 @@
 import '../css/Navbar.css'
 import netflix from '../assets/netflix.png'
 
-function Navbar() {
+function Navbar({ SideBar, setSideBar }) {
     return (
         <nav className="navbar">
             <div className="left-links">
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-list" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-list" viewBox="0 0 16 16" onClick={() => setSideBar(SideBar ? false : true)}>
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
                 <a href='/'><img src={netflix} width="100px" alt='netflix logo'></img></a>
